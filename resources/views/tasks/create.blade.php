250a000e7cf88cd553b97f75acb2c36956d50fb5
yasuko_sekine <kuronene23@gmail.com>
2018-02-22 12:35:16 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,17 +4,24 @@
 
   <h1>新規タスク作成ページ</h1>
   
-  {!! Form::model($task, ['route' => 'tasks.store']) !!}
+  <div class=row>
+    <div class="col-sm-offset-2 col-sm-8 col-lg-offset-3 col-lg-6">
+      {!! Form::model($task, ['route' => 'tasks.store']) !!}
+      
+        <div class="form-group">
+          {!! Form::label('status', 'ステータス:') !!}
+          {!! Form::text('status', null, ['class' => 'form-control']) !!}  
+        </div>
+        
+        <div class="form-group">
+          {!! Form::label('content', 'タスク:') !!}
+          {!! Form::text('content', null, ['class' => 'form-control']) !!}
+        </div>
+        
+          {!! Form::submit('作成', ['class' => 'btn btn-primary']) !!}
+        
+      {!! Form::close() !!}      
+    </div>
+  </div>
   
-    {!! Form::label('status', 'ステータス:') !!}
-    {!! Form::text('status') !!}  
-  
-  
-    {!! Form::label('content', 'タスク:') !!}
-    {!! Form::text('content') !!}
-    
-    {!! Form::submit('作成') !!}
-    
-  {!! Form::close() !!}
-    
 @endsection
