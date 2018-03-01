c8f847a14304a170187c71f024041524cb57d274
yasuko_sekine <kuronene23@gmail.com>
2018-03-01 08:01:02 +0000

    user registration

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,7 @@ Route::get('/', function() {
   return view('welcome');
 });
 
-
+Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
+Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
 
 Route::resource('tasks', 'TasksController');
